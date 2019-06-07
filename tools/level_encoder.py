b8884f7daf30e7c48536a062bdc15986bd50ac50
Frisé <lefrise@gmail.com>
2019-06-07 13:26:47 +0200

    Encoding all levels
    Improved level_encoder and encoded all previously existing levels with RLE

--- tools/level_encoder.py
+++ tools/level_encoder.py
@@ -14,15 +14,14 @@ class Cell:
 
     def __str__(self):
         return "{{{0},{1}}}".format(self.number, self.count)
-    def __repr__(self):
-        return self.__str__()
+    def tostr(self):
+        return self.__str__() + ", "
 
 #----------FUNCTIONS----------
 
 def encode_level(level):
 
-    encoded_level = []
-    encoded_string = ""
+    encoded_string = "{\n    "
 
     for j in range(len(level)):
 
@@ -32,27 +31,44 @@ def encode_level(level):
             if level[j][i] != number:
 
                 if(number != -1) :
-                    encoded_level.append(Cell(number, count))
-                    encoded_string += str(Cell(number, count))
-                    encoded_string += ", "
+                    encoded_string += Cell(number, count).tostr()
 
                 number = level[j][i]
                 count = 1
             else:
                 count += 1
 
-        encoded_level.append(Cell(number, 0xFF))
-        encoded_string += "{{{0},0xFF}}\n".format(number)
+        if(count > 1):
+            encoded_string += Cell(number, count).tostr()
+           
+        encoded_string += "{{{0},0xFF}}".format(number)
 
-    return encoded_level, encoded_string
+        if j == len(level) - 1:
+            encoded_string += "\n"
+        else:
+            encoded_string += ",\n    "
+
+    return encoded_string + "};"
 
 
 
 
 #----------MAIN---------------
 
-encoded_str = encode_level(LEVEL1)[1]
-print ("level1:")
+encoded_str = encode_level(LEVEL0)
+print ("PROGMEM static const Cell LEVEL_0[] =")
+print (encoded_str)
+
+encoded_str = encode_level(LEVEL1)
+print ("PROGMEM static const Cell LEVEL_1[] =")
+print (encoded_str)
+
+encoded_str = encode_level(LEVEL2)
+print ("PROGMEM static const Cell LEVEL_2[] =")
+print (encoded_str)
+
+encoded_str = encode_level(LEVEL3)
+print ("PROGMEM static const Cell LEVEL_3[] =")
 print (encoded_str)
 
 
